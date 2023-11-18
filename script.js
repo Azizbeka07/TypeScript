5b1c8ea0e819af699189e16923d36224bacf9d10
Azizbek <a@gmail.com>
2023-11-18 18:21:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// const firstName: string = "Azizbek ";
+// let lastName: string = "Abdrimov ";
+// const x: number = 1;
+// const boo: boolean = true;
+// console.log(firstName + lastName + boo + x);
+// const num: null = null;
+var colors = ["red", "white"];
+console.log(colors);
---
+++ script.ts
@@ -0,0 +1,20 @@
+// const firstName: string = "Azizbek ";
+
+// let lastName: string = "Abdrimov ";
+
+// const x: number = 1;
+
+// const boo: boolean = true;
+
+// console.log(firstName + lastName + boo + x);
+
+
+// const num: null = null;
+
+const colors: string[] = ["red", "white"] 
+
+console.log(colors);
+
+let smth: any = 1;
+ 
+smth = 'Azizbek';
